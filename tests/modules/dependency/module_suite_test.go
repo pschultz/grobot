54b07adfa888a052e4a7a360062ac438fbaf2448
Friedrich Große <friedrich.grosse@classmarkets.com>
2014-11-21 21:27:17 +0100

    Only show test log output if spec failed

--- tests/modules/dependency/module_suite_test.go
+++ tests/modules/dependency/module_suite_test.go
@@ -4,10 +4,13 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/fgrosse/grobot/log"
+	"github.com/fgrosse/grobot/tests/testAPI"
 	"testing"
 )
 
 func TestGrobot(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Dependency module test suite")
+	log.SetOutputWriter(new(testAPI.TestLogger))
+	RegisterFailHandler(testAPI.LoggedFailHandler)
+	RunSpecsWithDefaultAndCustomReporters(t, "Dependency module test suite", []Reporter{&testAPI.LoggedReporter{}})
 }
